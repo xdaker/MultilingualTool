a08ae3e9edbee73ecc2d93db88524eea27e3cd3f
xdaker <xdaker@qq.com>
2017-07-20 11:49:22 +0800

    创建

---
+++ MultilingualTool/FlieHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilingualTool
+{
+    public class FlieHelper
+    {
+
+        private static List<string> GetAllFliePath(string path)
+        {
+            DirectoryInfo TheFolder = new DirectoryInfo(path);
+            List<string> list = new List<string>();
+            //遍历文件夹
+            foreach (DirectoryInfo nextFolder in TheFolder.GetDirectories())
+            {
+                foreach (FileInfo nextFile in nextFolder.GetFiles())
+                {
+                    list.Add(nextFile.FullName);
+                }
+                foreach (var directoryInfo in nextFolder.GetDirectories())
+                {
+                    list.AddRange(GetAllFliePath(directoryInfo.FullName));
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ MultilingualTool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilingualTool
+{
+    class Program
+    {
+        public static TransApi TransApi = new TransApi("2015063000000001", "1435660288");
+        static void Main(string[] args)
+        {
+           
+            string result = TransApi.GetTransResult("你好", "auto", "auto");
+            Console.ReadKey();
+        }
+    }
+}
